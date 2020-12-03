f9f1cc281f43fa473e5332d9dc31f15bdd91e2b5
Hanahime1075 <ngduykhuong0503@gmail.com>
2020-12-03 17:25:00 +0700

    processing ...

---
+++ ComedyEvents/Controllers/EventsController.cs
@@ -0,0 +1,36 @@
+﻿using ComedyEvents.Model;
+using ComedyEvents.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComedyEvents.Controllers
+{
+    [Route("api/controller")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        private readonly IEventRepository _eventRepository;
+
+        public EventsController(IEventRepository eventRepository)
+        {
+            _eventRepository = eventRepository;
+        }
+
+        public async Task<ActionResult<Event[]>> Get (bool includeGigs = false)
+        {
+            try
+            {
+                var results = await _eventRepository.GetEvents(includeGigs);
+                return Ok(results);
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+        }
+    }
+}
