8c3efa0d23742adc3b57d9248ffdbdb5cac3f459
Hanahime1075 <ngduykhuong0503@gmail.com>
2020-12-08 09:22:45 +0700

    complete image table

---
+++ eShopSolution.Data/Migrations/20201208022214_ImageTable.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eShopSolution.Data.Migrations
+{
+    public partial class ImageTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "OrderDate",
+                table: "Orders",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2020, 12, 7, 14, 56, 38, 129, DateTimeKind.Local).AddTicks(4297));
+
+            migrationBuilder.CreateTable(
+                name: "ProductImages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    ImagePath = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Caption = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    IsDefault = table.Column<bool>(type: "bit", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    SortOrder = table.Column<int>(type: "int", nullable: false),
+                    FileSize = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductImages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProductImages_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "AppRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("8d04dce2-969a-435d-bba4-df3f325983dc"),
+                column: "ConcurrencyStamp",
+                value: "dbeb1f20-b4ed-4c9c-93c3-802aad22acde");
+
+            migrationBuilder.UpdateData(
+                table: "AppUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("69bd714f-9576-45ba-b5b7-f00649be00de"),
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "e98a1465-adf3-4338-aa3f-a14e184ccaf1", "AQAAAAEAACcQAAAAEOP8c49YZ4JbHFXWm6bbGi/yoBnQrw+UEkL6w7U8XxGJJeEKFKzhOyuG6GdnK2AjxA==" });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateCreated",
+                value: new DateTime(2020, 12, 8, 9, 22, 12, 964, DateTimeKind.Local).AddTicks(2504));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductImages_ProductId",
+                table: "ProductImages",
+                column: "ProductId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductImages");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "OrderDate",
+                table: "Orders",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2020, 12, 7, 14, 56, 38, 129, DateTimeKind.Local).AddTicks(4297),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.UpdateData(
+                table: "AppRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("8d04dce2-969a-435d-bba4-df3f325983dc"),
+                column: "ConcurrencyStamp",
+                value: "d5c07db0-4101-420d-a678-e98d9ee33973");
+
+            migrationBuilder.UpdateData(
+                table: "AppUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("69bd714f-9576-45ba-b5b7-f00649be00de"),
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "e0b9b87d-0e05-4949-b90b-107c1a7748ca", "AQAAAAEAACcQAAAAEFVBl8HVrKUAxIH/j/y7nG17ZmABcy0e1vWoyKDPhwHhzLtfsvowGfuFm6G/XCN7CA==" });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateCreated",
+                value: new DateTime(2020, 12, 7, 14, 56, 38, 150, DateTimeKind.Local).AddTicks(9227));
+        }
+    }
+}
